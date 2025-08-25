1281c2db548f7ea6746c3b907f701ea9320efdc4
sDupsy <123974978+sDupsy@users.noreply.github.com>
2025-08-25 14:05:04 +0200

    Add Book Display as Cards

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const cards = document.querySelector(".cards");
 
 function Book(title, author, pages, read, id) {
   this.title = title;
@@ -17,4 +18,22 @@ function addBookToLibrary(title, author, pages, read) {
   const book = new Book(title, author, pages, read, id);
   myLibrary.push(book);
   return book;
+}
+
+addBookToLibrary("12 Rules for life", "Jordan Peterson", 448, true);
+addBookToLibrary("Factfulness", "Hans, Anna and Ola Rosling", 352, true);
+addBookToLibrary("IT", "Stephen King", 1168, false);
+
+console.log(myLibrary);
+
+for (let book of myLibrary) {
+  const newBook = document.createElement("div");
+  newBook.classList.add("card");
+  newBook.innerHTML = `
+    <p>Title: ${book.title}</p>
+    <p>Author: ${book.author}</p>
+    <p>Pages: ${book.pages}</p>
+    <p>${book.read ? "Already read" : "Not read yet"}</p>
+  `;
+  cards.appendChild(newBook);
 }
